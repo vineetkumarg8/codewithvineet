0da9155cb2825e58bb2b37bf71fd44119629a8d3
vineet kumar <vineetkumar12392@gmail.com>
2025-04-11 23:50:08 +0530

    Update DFS of Graph.cpp

--- Graph/DFS_BFS/DFS of Graph.cpp
+++ Graph/DFS_BFS/DFS of Graph.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+    public:
+    
+    void dfs(int u,vector<vector<int>> &adj, 
+    vector<bool> &visited,vector<int>&result){
+        if(visited[u] == true) return;
+        visited[u] = true;
+        result.push_back(u);
+        
+        for(int &v : adj[u]){
+            if(!visited[v]){
+                dfs(v,adj,visited,result);
+            }
+        }
+    }
+    
+    vector<int> dfs(vector<vector<int>>adj) {
+        int V = adj.size(); // get number of vertices from the size of adj
+        vector<int>result;
+        vector<bool> visited(V,false);
+        dfs(0, adj,visited,result);
+        return result;
+    }
+};

5861348ab0e64b0923a8f6644fa7392efb9fe7c4
vineet kumar <vineetkumar12392@gmail.com>
2025-05-02 18:54:44 +0530

    Update palindrome.cpp

--- String/palindrome.cpp
+++ String/palindrome.cpp
@@ -21,3 +21,26 @@ bool palindrome(string &s, int i, int j) {
     return false;
 }
 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str = "madam";
+    bool isPalindrome = true;
+
+    for (int i = 0; i < str.size() / 2; i++) {
+        if (str[i] != str[str.size() - 1 - i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+        cout << "Palindrome" << endl;
+    else
+        cout << "Not a palindrome" << endl;
+
+    return 0;
+}
+
